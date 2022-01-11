b9082160373c980d5615ead3d2f90a092003f371
Michael Royal <45159366+mikeroyal@users.noreply.github.com>
2022-01-10 18:42:58 -0800

    Updated file.

--- Getting started with Godot.gd
+++ Getting started with Godot.gd
@@ -1,1 +1,18 @@
 Godot Engine code samples & snippets coming soon!
+
+
+# A file is a class.
+
+# Inheritance
+
+extends BaseClass
+
+# class definition with a custom icon (optional) 
+
+class_name MyClass, "res://path/to/optional/sample-icon.svg"
+
+# Member variables
+
+var s = "Hello"
+
+print(s)
